d35eb4b4ae36ba018adcc4584332b67fdda78843
AleScla <scla.alessandro@gmail.com>
2024-07-23 12:44:33 +0200

    esercizio completato, da aggiungere commenti

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,33 @@
 
 const btn = document.querySelector('button');
 const container = document.querySelector('.container');
+const h3 = document.querySelector('h3');
 
 btn.addEventListener('click', function(){
     let difficulties = document.getElementById('difficulties').value;
     console.log(difficulties);
     container.innerHTML = '';
-    
-    boxPrinter(difficulties);
-    
-    
-    
+
+    boxPrinter(difficulties); // stampo i box in pagina 
+    h3.innerHTML = '';
 })
 
 // funzioni
 function boxPrinter (difficolta){
     let num = 0;
-    
 
-    if(difficolta == 'normal'){
-        num = 100;
+    if(difficolta == 'normal'){ // trasforma il valore di num in base alla difficoltà
+        num = 100; 
         
     }
-
     else if (difficolta == 'hard'){
         num = 81;
         
     }
-
     else{
         num = 49;
         
     }
-
-    for (let i = 1; i <= num; i++){
-        let col = document.createElement('div');
-        col.innerHTML = i ;
-        col.classList.add(difficolta);
-        container.append(col);
-        col.addEventListener('click', function(){
-            col.classList.add('lightcoral');
-            console.log(i);
-        })
-    }
-
     let bombs = [];
     let k = 1;
     while (k <= 16){
@@ -53,10 +37,37 @@ function boxPrinter (difficolta){
             k++;
         }
     }
-    console.log('le bombe sono:', bombs);
-
-    return num
-}
+    console.log('le bombe sono:', bombs); 
+    
+    let valore;
+    let contatore = 0;
+    let WinOrLose = true;
+    for (let i = 1; i <= num; i++){
+        
+        let col = document.createElement('div');
+        col.innerHTML = i ;
+        col.classList.add(difficolta);
+        container.append(col);
+        
+        col.addEventListener('click', function(){
+            
+            if (WinOrLose){
+                if(!col.classList.contains('lightcoral')){
+                    col.classList.add('lightcoral');
+                    contatore += 1; 
+                }
+                valore = i;
+                
+                if(bombs.includes(valore)){
+                    col.style.backgroundColor = 'red';
+                    contatore -= 1;
+                    h3.innerHTML = ('Hai perso, il tuo punteggio è: ' + contatore);
+                    WinOrLose = false;
+                }
+            }
+        })  
+    }    
+}    
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
